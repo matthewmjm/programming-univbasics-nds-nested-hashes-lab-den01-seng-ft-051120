a4c4abd4d95f0383545cde13cd022a02a5fb1ba9
Matthew Malecki <mmalecki4@gmail.com>
2020-04-17 14:46:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,14 +6,42 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
+#def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+#end
+
+#def literal_aoh
+  # Using Array literal syntax only, build a nested array that uses the data in
+  # held in the constants
+#end
+
+#def aoh_lookup(aoh, row, key)
+#end
+
+#def aoh_update(aoh, row, key, new_value)
+  # Update the AoH data at row and key to have the value of new_value
+  # Return the updated AoH
+#end
+
+def assembled_aoh
+  assembled_aoh = Array.new
+  assembled_aoh = assembled_aoh.push(DON_G)
+    assembled_aoh = assembled_aoh.push(JOELLE_VD)
+      assembled_aoh = assembled_aoh.push(PAT_M)
+        assembled_aoh = assembled_aoh.push(KATE_G)
+          assembled_aoh = assembled_aoh.push(BRUCE_G)
+  assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  literal_aoh = []
+  literal_aoh = literal_aoh.push(DON_G.sort)
+    literal_aoh = literal_aoh.push(DON_G.sort)
+      literal_aoh = literal_aoh.push(DON_G.sort)
+        literal_aoh = literal_aoh.push(DON_G.sort)
+          literal_aoh = literal_aoh.push(DON_G.sort)
+  literal_aoh  
 end
 
 def aoh_lookup(aoh, row, key)
